26dc54dd5cbcbb3186f4c82d777d84efbfe5feb7
Peter Schuller <peter.schuller@infidyne.com>
2011-10-22 19:39:32 +0000

    Duh, actually build in .build dir.

--- makerepo.sh
+++ makerepo.sh
@@ -6,8 +6,8 @@ set -x
 # In order to faciliate re-running this script cleanly, perform builds in a copy.
 rm -rf packages.build
 (cd packages && find . -depth -print | cpio -pdmv --sparse ../packages.build)
-(cd packages/ec2-metadata && makepkg --asroot)
-(cd packages/linux-arch4ec2 && makepkg --asroot)
+(cd packages.build/ec2-metadata && makepkg --asroot)
+(cd packages.build/linux-arch4ec2 && makepkg --asroot)
 
 mkdir -p repo
 cp packages.build/ec2-metadata/*.pkg.tar.xz repo
