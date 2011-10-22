5f4124972d6b74a6dbbfcdb145d4641b8d6a7efb
Peter Schuller <peter.schuller@infidyne.com>
2011-10-22 19:35:19 +0000

    Build pkgs in dedicated dir to avoid polluting src.

--- makerepo.sh
+++ makerepo.sh
@@ -3,12 +3,15 @@
 set -e
 set -x
 
+# In order to faciliate re-running this script cleanly, perform builds in a copy.
+rm -rf packages.build
+(cd packages && find . -depth -print | cpio -pdmv --sparse ../packages.build)
 (cd packages/ec2-metadata && makepkg --asroot)
 (cd packages/linux-arch4ec2 && makepkg --asroot)
 
 mkdir -p repo
-cp packages/ec2-metadata/*.pkg.tar.xz repo
-cp packages/linux-arch4ec2/*.pkg.tar.xz repo
+cp packages.build/ec2-metadata/*.pkg.tar.xz repo
+cp packages.build/linux-arch4ec2/*.pkg.tar.xz repo
 
 (cd repo && repo-add arch4ec2.tar.gz *.pkg.tar.xz)
 
